46efe45edce4ffacd2dd099f65141d006a4e49bf
LimaXRomeo <lixinrui0001@hotmail.com>
2019-10-09 15:09:19 +0800

    1.1 Update 10/9/2019

--- TRIAREA.C
+++ TRIAREA.C
@@ -4,29 +4,35 @@ int main()
 {
     double a,b,c,d,s,t;
     int e,i=1;
-    printf("--------- TriangleArea ---------\n          By LimaXRomeo         \n");
+    printf("\n--------- TriangleArea ---------\n          By LimaXRomeo         \n");
     printf("Please input the lengths of three sides of your triangle.\nI'll tell you its area.\n");
     while (i==1)
     {
 	printf("a=");
-	scanf("%lf", &a);
+	scanf("%lg", &a);
 	printf("b=");
-	scanf("%lf",&b);
+	scanf("%lg",&b);
 	printf("c=");
-	scanf("%lf",&c);
+	scanf("%lg",&c);
 	d=(a+b+c)/2.0;
 	t=d*(d-a)*(d-b)*(d-c);
 	s=sqrt(t);
-	printf("\nS=%lf\n", s);
-	printf("Press 0 and then ENTER to exit, or type other NUMBERS then ENTER to continue.\n");
-	scanf("%d",&e);
+	printf("\nS=%lg\n", s);
+	printf("Type 0 and then ENTER to exit, or type 1 then ENTER to continue.\n");
+    loop:scanf("%d",&e);
 	if (e==0)
 	{
 	    return 0;
 	}
-	else
+	else if(e==1)
 	{
 	    printf("\nContinue.\n");
 	}
+	else
+	{
+	    printf("Invalid!\n");
+	    goto loop;
+	}
     }
+return 0;
 }
