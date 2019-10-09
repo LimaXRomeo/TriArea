2d7ca3c9cb2b9637d82f9abbff977936be2d426b
LimaXRomeo <lixinrui0001@hotmail.com>
2019-10-09 16:42:46 +0800

    1.2 Update 10/9/2019

--- TRIAREA.C
+++ TRIAREA.C
@@ -2,7 +2,7 @@
 #include <math.h>
 int main()
 {
-    double a,b,c,d,s,t;
+    long double a,b,c,d,s,t;
     int e,i=1;
     printf("\n--------- TriangleArea ---------\n          By LimaXRomeo         \n");
     printf("Please input the lengths of three sides of your triangle.\nI'll tell you its area.\n");
