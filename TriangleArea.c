6b009a1549233aaf98a32e62c284c826a6070a73
LimaXRomeo <lixinrui0001@hotmail.com>
2019-10-09 14:45:26 +0800

    1.1 Update 10/9/2019

--- TriangleArea.c
+++ TriangleArea.c
@@ -4,31 +4,36 @@
 int main()
 {
 	system("color 3F");
-	double a, b, c, d, s, t;
+	long double a, b, c, d, s, t;
 	int k, i = 1;
 	while (i == 1)
 	{
 		printf("--------- TriangleArea ---------\n          By LimaXRomeo         \n");
 		printf("Please input the lengths of three sides of your triangle.\n I'll tell you its area.\n");
 		printf("a=");
-		scanf_s("%lf", &a);
+		scanf_s("%lg", &a);
 		printf("b=");
-		scanf_s("%lf", &b);
+		scanf_s("%lg", &b);
 		printf("c=");
-		scanf_s("%lf", &c);
+		scanf_s("%lg", &c);
 		d = (a + b + c) / 2.0;
 		t = d * (d - a) * (d - b) * (d - c);
 		s = sqrt(t);
-		printf("\nS=%lf\n\n", s);
-		printf("Press 0 and then ENTER to exit, or type other NUMBERS then ENTER to continue.\n");
-		scanf_s("%d", &k);
+		printf("\nS=%lg\n\n", s);
+		printf("Type 0 and then ENTER to exit, or type 1 then ENTER to continue.\n");
+	loop:scanf_s("%d", &k);
 		if (k == 0)
 		{
 			return 0;
 		}
-		else
+		else if (k == 1)
 		{
 			system("cls");
+		}
+		else
+		{
+			printf("Invalid!\n");
+			goto loop;
 		}
 	}
 }
